06a3ec3e1e1333d8b408fe6e7b9cd99fb4f106ea
Simone <150595274+simonetimi@users.noreply.github.com>
2024-02-02 00:37:03 +0100

    feat: add styling and fix form to avoid submit if field is empty

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,35 @@ import { format } from 'date-fns';
 
 const router = express.Router();
 
+const colors = [
+  '#00b159',
+  '#d11141',
+  '#bf509c',
+  '#ffc425',
+  '#f37735',
+  '#8c311f',
+];
+
 export const messages = [
   {
     text: 'Hi Bob!',
-    author: 'Martin',
-    added: format(new Date(), 'dd MMMM yyyy, HH:mm'),
+    author: 'Danielle Poole',
+    added: format('1992-07-03T18:19:27.052Z', 'dd MMMM yyyy, HH:mm'),
   },
   {
-    text: 'Ciao vita!',
-    author: 'Costanza',
-    added: format(new Date(), 'dd MMMM yyyy, HH:mm'),
+    text: 'Well, sir, I don’t wear pants.',
+    author: 'Margo Madison',
+    added: format('1973-12-31T22:19:27.052Z', 'dd MMMM yyyy, HH:mm'),
   },
 ];
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Mini Message Board', messages: messages });
+  res.render('index', {
+    title: 'Mini Message Board',
+    messages: messages,
+    colors: colors,
+  });
 });
 
 export default router;
--- routes/new.js
+++ routes/new.js
@@ -5,16 +5,17 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-  res.render('form', { title: 'Leave a message' });
+  res.render('form', { title: 'Leave a message', app: 'Mini Message Board' });
 });
 
 router.post('/', function (req, res) {
   const author = req.body.author;
   const message = req.body.message;
+  const date = format(new Date(), 'dd MMMM yyyy, HH:mm');
   messages.push({
     text: message,
     author: author,
-    date: format(new Date(), 'dd MMMM yyyy, HH:mm'),
+    added: date,
   });
   res.redirect('/');
 });
