c874ea872939f1c1b5fa0a1a857a2dc41ae43649
Simone <150595274+simonetimi@users.noreply.github.com>
2024-02-01 22:38:11 +0100

    feat: add basic logic to show and post user messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
-var router = express.Router();
+import { format } from 'date-fns';
+
+const router = express.Router();
+
+export const messages = [
+  {
+    text: 'Hi Bob!',
+    author: 'Martin',
+    added: format(new Date(), 'dd MMMM yyyy, HH:mm'),
+  },
+  {
+    text: 'Ciao vita!',
+    author: 'Costanza',
+    added: format(new Date(), 'dd MMMM yyyy, HH:mm'),
+  },
+];
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 export default router;
--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import { format } from 'date-fns';
+import { messages } from './index.js';
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+  res.render('form', { title: 'Leave a message' });
+});
+
+router.post('/', function (req, res) {
+  const author = req.body.author;
+  const message = req.body.message;
+  messages.push({
+    text: message,
+    author: author,
+    date: format(new Date(), 'dd MMMM yyyy, HH:mm'),
+  });
+  res.redirect('/');
 });
 
 export default router;
