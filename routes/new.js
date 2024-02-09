7bfd02a5b34e3c4574dc1e7e8ad5a3786dd87155
Simone <150595274+simonetimi@users.noreply.github.com>
2024-02-09 20:25:08 +0100

    style: remove unused imports

--- routes/new.js
+++ routes/new.js
@@ -1,14 +1,9 @@
 import express from 'express';
 import { format } from 'date-fns';
 
-// this import is the array with messages. it must be replaced with a database
-// import { messages } from './index.js';
-
-import { body, validationResult } from 'express-validator';
 import asyncHandler from 'express-async-handler';
 
 import Message from '../models/message.js';
-import User from '../models/user.js';
 
 const router = express.Router();
 
